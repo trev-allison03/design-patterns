3398b154ce60b6cf8cb013b6e6cb94d43a8519a2
Trevor Allison <trevorallison@me.com>
2023-02-02 11:52:53 -0500

    Removing extra blank lines from output and adding output.png

--- factory/ToyStore.java
+++ factory/ToyStore.java
@@ -12,8 +12,8 @@ public abstract class ToyStore {
     public String orderPuzzle(String type){
         Puzzle puzzle = createPuzzle(type);
         String result = "";
-        result += puzzle.assemble()+"\n";
-        result += puzzle.boxPuzzle()+"\n";
+        result += puzzle.assemble();//+"\n";
+        result += puzzle.boxPuzzle();//+"\n";
         return result;
     }
     /**
