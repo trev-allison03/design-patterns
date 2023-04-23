68b4f9de771db70ea469b951b3f705f6c1ee8b78
Trevor Allison <trevorallison@me.com>
2023-04-23 18:44:18 -0400

    Java doccing

--- adapter/MediaAdapter.java
+++ adapter/MediaAdapter.java
@@ -1,5 +1,8 @@
 package adapter;
-
+/**
+ * @author Trevor Allison
+ * Media adapter class for adapter project, interface to connect little media and media
+ */
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -10,12 +13,24 @@ import java.util.Date;
 public class MediaAdapter implements Media{
     private LittleMedia littleMedia;
 
+    /**
+     * Constructor for media adapter
+     * @param littleMedia
+     */
     public MediaAdapter(LittleMedia littleMedia){
         this.littleMedia = littleMedia;
     }
+    /**
+     * get title method
+     * @return Title of media
+     */
     public String getTitle(){
         return this.littleMedia.getTitle();
     }
+    /**
+     * get author method, splits up string username into first and last name to create author object
+     * @return author
+     */
     public Author getAuthor(){
         String name = this.littleMedia.getAuthor();
         String[] strings = name.split(" ");
@@ -24,12 +39,28 @@ public class MediaAdapter implements Media{
         Author author = new Author(firstName, lastName);
         return author;
     }
+    /**
+     * get description method
+     * @return description of media
+     */
     public String getDescription(){
         return this.littleMedia.getDescription();
     }
+    /**
+     * add review method
+     * @param firstName
+     * @param lastName
+     * @param date
+     * @param rating
+     * @param comment
+     */
     public void addReview(String firstName, String lastName, Date date, double rating, String comment){
         getReviews().add(new Review(firstName, lastName, date, rating, comment));
     }
+    /**
+     * get reviews method 
+     * @return Arraylist of reviews
+     */
     public ArrayList<Review> getReviews(){
         ArrayList<Review> reviews = new ArrayList<Review>();
         for(String review : this.littleMedia.getReviews()){
@@ -47,7 +78,11 @@ public class MediaAdapter implements Media{
         }
         return reviews;
     }
-
+    /**
+     * private helper method to parse date
+     * @param dateString
+     * @return date
+     */
     private Date parseDate(String date) {
         try {
             return new SimpleDateFormat("yyyy-MM-dd").parse(date);
